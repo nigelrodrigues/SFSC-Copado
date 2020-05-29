2cb3d239430f707753710e0dcbe79e95cce63d86
Admin Traction <admin@tractionondemand.com>
2020-05-29 06:08:08 +0000

    US-0000100: Issue a Basic Appeasement

--- aura/trac_IssueAppeasement/trac_IssueAppeasementController.js
+++ aura/trac_IssueAppeasement/trac_IssueAppeasementController.js
@@ -5,22 +5,58 @@
  */
 ({
     doInit: function(cmp, event, helper) {
+        cmp.set('v.isSpinner', true);
+        cmp.set('v.isLoading', false);
         helper.calculateDisplayAmounts(cmp);
     },
     selectAmount: function(cmp, event, helper) {
-        let selectedLabel = event.getSource().get("v.label");
-        let amount = parseInt(selectedLabel.replace(",",""));
-        console.log("amount: ", amount);
-        cmp.set("v.selectedAmount", amount);
-        cmp.set("v.selectedLabel", selectedLabel);
         helper.calculateDisplayAmounts(cmp);
     },
-    submitAppeasement: function(cmp, event, helper) {
+    submitAppeasementForm: function(cmp, event, helper) {
+        cmp.set('v.isLoading', true);
         var selectedAmount = cmp.get("v.selectedAmount");
         console.log("Submitting appeasement of " + selectedAmount + " points");
 
         // submit to server
+        var action = cmp.get('c.submitAppeasement');
 
-        // handle response
+        var params = {
+            contactId: cmp.get('v.contactId'),
+            points: cmp.get('v.selectedAmount'),
+            pointsAvailable: cmp.get('v.pointsAvailable')
+        };
+        console.log('params: ', params);
+        action.setParams(params);
+
+        action.setCallback(this, function (response) {
+            console.log('inside action setCallback; response: ', response);
+            cmp.set('v.isLoading', false);
+            var state = response.getState();
+
+            console.log('cmp.isValid: ' + cmp.isValid() + '; state: ' + state);
+            if (cmp.isValid() && state === "SUCCESS") {
+                var result = response.getReturnValue();
+                console.log('result: ', result);
+
+                if (typeof result !== undefined && result != null) {
+                    if (result.isSuccess) {
+                        // success, show success toast
+                        helper.showToast(result.message, 'success', 'Appeasement Submitted');
+
+                        // fire event to close the modal
+                        helper.fireCloseModalEvent();
+                    } else {
+                        // failure, show error message
+                        cmp.set("v.isError", true);
+                        cmp.set("v.errorMsg", result.message);
+                    }
+                } else {
+                    // unexpected error
+                    cmp.set("v.isError", true);
+                    cmp.set("v.errorMsg", "Connection Error");
+                }
+            }
+        });
+        $A.enqueueAction(action);
     }
 });
--- aura/trac_IssueAppeasement/trac_IssueAppeasementHelper.js
+++ aura/trac_IssueAppeasement/trac_IssueAppeasementHelper.js
@@ -3,15 +3,32 @@
  */
 
 ({
+    showToast: function(message, type, title) {
+        var resultsToast = $A.get("e.force:showToast");
+        resultsToast.setParams({
+            "title": title,
+            "message": message,
+            "type" : type
+        });
+        resultsToast.fire();
+    },
     calculateDisplayAmounts: function(cmp) {
         var selectedAmount = cmp.get('v.selectedAmount');
         if (!$A.util.isEmpty(selectedAmount)) {
+            selectedAmount = parseInt(selectedAmount);
+            cmp.set('v.selectedLabel', this.commaFormatInteger(selectedAmount));
+
             var displayAmount = '$' + (selectedAmount / 200).toFixed(2) + ' CAD';
             cmp.set('v.displayAmount', displayAmount);
 
-            var finalBalance = this.commaFormatInteger(cmp.get('v.currentBalance') + selectedAmount);
+            var finalBalance = this.commaFormatInteger(parseInt(cmp.get('v.pointsAvailable')) + parseInt(selectedAmount));
             cmp.set('v.finalDisplayBalance', finalBalance);
         }
+    },
+    fireCloseModalEvent: function() {
+        var appEvent = $A.get("e.c:trac_CloseModalApplicationEvent");
+        appEvent.setParams({"closeModal" : true});
+        appEvent.fire();
     },
     commaFormatInteger: function(intNum) {
         var str = intNum.toString();
