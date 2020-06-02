8c021b661520d1b271d2ef4fb18b48214473a57f
Admin Traction <admin@tractionondemand.com>
2020-06-02 21:14:44 +0000

    US-0000115: Harness Application for Production Deployments

--- aura/trac_IssueAppeasement/trac_IssueAppeasementHelper.js
+++ aura/trac_IssueAppeasement/trac_IssueAppeasementHelper.js
@@ -24,6 +24,49 @@
             cmp.set('v.finalPointBalance', finalBalance);
         }
     },
+    submitAppeasement: function(cmp, event, helper) {
+        cmp.set('v.showSpinner', true);
+        var appeasePoints = cmp.get("v.appeasePoints");
+
+        // submit to server
+        var action = cmp.get('c.submitAppeasement');
+
+        var params = {
+            loyaltyNumber: cmp.get('v.loyalty.external_customer_id'),
+            email: cmp.get('v.loyalty.email'),
+            points: cmp.get('v.appeasePoints'),
+            pointsAvailable: cmp.get('v.pointsAvailable')
+        };
+        action.setParams(params);
+
+        action.setCallback(this, function (response) {
+            cmp.set('v.showSpinner', false);
+            var state = response.getState();
+
+            if (cmp.isValid() && state === "SUCCESS") {
+                var result = response.getReturnValue();
+
+                if (typeof result !== undefined && result != null) {
+                    if (result.isSuccess) {
+                        // success, show success toast
+                        helper.showToast(result.message, 'success', 'Appeasement Submitted');
+
+                        // fire event to close the modal
+                        helper.fireCloseModalEvent();
+                    } else {
+                        // failure, show error message
+                        cmp.set("v.isError", true);
+                        cmp.set("v.errorMsg", result.message);
+                    }
+                } else {
+                    // unexpected error
+                    cmp.set("v.isError", true);
+                    cmp.set("v.errorMsg", "Connection Error");
+                }
+            }
+        });
+        $A.enqueueAction(action);
+    },
     fireCloseModalEvent: function() {
         var appEvent = $A.get("e.c:trac_CloseModalApplicationEvent");
         appEvent.setParams({"closeModal" : true});
