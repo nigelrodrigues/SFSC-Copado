6e75be172ba8244c54c232a94945da5c2502e733
Bryan Cantos <bryan.cantos@hbc.com>
2020-06-08 18:49:36 +0000

    US-0000118: Error Handling for Merkle Communication

---
+++ aura/trac_CloseLoyaltyAccount/trac_CloseLoyaltyAccountController.js
@@ -0,0 +1,34 @@
+({
+
+    init: function (component, event, helper) {
+        var status = component.get('v.loyalty.status')
+        if(status == 'paused')
+            component.set('v.isClose', true);
+    },
+
+   openModel: function(component, event, helper) {
+      // Set isModalOpen attribute to true
+      component.set("v.isModalOpen", true);
+   },
+  
+   closeModel: function(component, event, helper) {
+      // Set isModalOpen attribute to false  
+      component.set("v.isModalOpen", false);
+   },
+  
+   closeAccount: function(component, event, helper) {
+       console.log('in close account')
+
+      if(component.get('v.isMerkleError') == true) {
+          component.set("v.isMerkleError", false);
+          component.set("v.isModalOpen", true);
+      }
+
+      var loyalty = component.get('v.loyalty')
+      helper.updateLoyaltyPoints(component, event, helper, loyalty, -Math.abs(loyalty.balance), 'closed_account')
+//      .then(() => helper.pauseAccount(component, event, helper, loyalty))
+      .catch(error => helper.handleError(component, error))
+
+//      helper.pauseAccount(component, event, helper, loyalty).catch(error => helper.handleError(component, error))
+   }
+})
