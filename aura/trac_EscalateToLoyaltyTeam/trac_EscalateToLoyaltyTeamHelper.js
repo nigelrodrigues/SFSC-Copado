49e75e89812dd94bb11f13e697b66ff3c4244559
Nigel Rodrigues <nrodrigues@tractionondemand.com>
2020-06-15 20:12:25 +0000

    US-0000093: Open a Loyalty Servicing Case

---
+++ aura/trac_EscalateToLoyaltyTeam/trac_EscalateToLoyaltyTeamHelper.js
@@ -0,0 +1,104 @@
+/**
+ * Created by nrodrigues on 6/10/2020.
+ */
+
+({
+    MAX_FILE_SIZE: 4500000,
+    CHUNK_SIZE: 750000,
+
+    showToast: function (title,message, type) {
+        var resultsToast = $A.get("e.force:showToast");
+        resultsToast.setParams({
+            "title": title,
+            "message": message,
+            "type": type
+        });
+        resultsToast.fire();
+    },
+
+    attachFile : function (component, event, helper) {
+
+        var fileInput = component.find("fileUpload").get("v.files");
+
+        if( fileInput ) {
+            var file = fileInput[0];
+            var self = this;
+
+            if (file.size > self.MAX_FILE_SIZE) {
+                component.set("v.fileName", 'Selected file size is too large. Maximum allowed file size is ' + (self.MAX_FILE_SIZE/1000000) + ' MB.\n' + ' Selected file size: ' + (file.size/1000000) + 'MB.');
+                return;
+            }
+
+            var objFileReader = new FileReader();
+            objFileReader.onload = $A.getCallback(function () {
+                var fileContents = objFileReader.result;
+                var base64 = 'base64,';
+                var dataStart = fileContents.indexOf(base64) + base64.length;
+                fileContents = fileContents.substring(dataStart);
+                self.uploadProcess(component, file, fileContents);
+            });
+
+            objFileReader.readAsDataURL(file);
+        }
+    },
+
+    uploadProcess: function(component, file, fileContents) {
+
+        var startPosition = 0;
+        var endPosition = Math.min(fileContents.length, startPosition + this.CHUNK_SIZE);
+        this.uploadInChunk(component, file, fileContents, startPosition, endPosition, '');
+    },
+
+
+    uploadInChunk: function(component, file, fileContents, startPosition, endPosition, attachId) {
+        var getchunk = fileContents.substring(startPosition, endPosition);
+        var action = component.get("c.attachToCase");
+        action.setParams({
+            parentId: component.get("v.recordId"),
+            contentType: file.type,
+            fileName: file.name,
+            fileContentsToEncode: encodeURIComponent(getchunk)
+        });
+
+        action.setCallback(this, function(response) {
+
+            var state = response.getState();
+            if (component.isValid() && state === "SUCCESS")
+            {
+
+                let result =  response.getReturnValue();
+                if (result == null)
+                {
+                    component.set("v.errorMsg", "Connection Error");
+                }
+                else
+                    {
+                        if(result.isSuccess)
+                        {
+                            startPosition = endPosition;
+                            endPosition = Math.min(fileContents.length, startPosition + this.CHUNK_SIZE);
+
+                            if (startPosition < endPosition)
+                            {
+                                this.uploadInChunk(component, file, fileContents, startPosition, endPosition, attachId);
+                            }
+                        }
+                        else
+                            {
+                            component.set("v.isError", true);
+                            component.set("v.errorMsg", result.message);
+                        }
+
+                }
+
+            }
+            else {
+                console.error("failed with state: " + state);
+            }
+
+        });
+
+        $A.enqueueAction(action);
+
+    }
+});
---
+++ aura/trac_LoyaltySearch/trac_LoyaltySearchController.js
@@ -0,0 +1,30 @@
+({
+    init: function (component, event, helper) {
+        component.set('v.options', [
+                                 { id: 1, label: 'Email', selected: true },
+                                 { id: 2, label: 'Loyalty Account Number' }
+                                // { id: 3, label: 'Phone Number' }
+                             ]);
+        component.set('v.selectedValue', '1');
+    },
+
+    loyaltySearch: function (component, event, helper) {
+        var loyalty = null;
+        var email = null;
+        var loyaltyId = null;
+        var phoneNum = null;
+
+        if(component.get('v.selectedValue') == '1' && helper.isNotBlank(component, 'emailInput')) {
+            email = component.find('emailInput').get("v.value");
+        } else if (component.get('v.selectedValue') == '2' && helper.isNotBlank(component, 'loyaltyNumberInput')) {
+            loyaltyId = component.find('loyaltyNumberInput').get("v.value");
+        } else if (component.get('v.selectedValue') == '3' && helper.isNotBlank(component, 'phoneNumberInput')) {
+            phoneNum = component.find('phoneNumberInput').get("v.value");
+        }
+
+        helper.getLoyalty(component, email, loyaltyId, phoneNum)
+        //.then(() => helper.getLoyaltyUAD(component, email, loyaltyId, phoneNum))
+        .catch(error => helper.handleError(component, error))
+    },
+
+});
