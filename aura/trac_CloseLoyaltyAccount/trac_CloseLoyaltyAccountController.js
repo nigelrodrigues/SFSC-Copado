02445c50b0694f742de777964c6561ac1a138410
Bryan Cantos <bryan.cantos@hbc.com>
2020-06-16 18:08:08 +0000

    US-0000102: Edit Loyalty Account Information (Name + email)

---
+++ aura/trac_CloseLoyaltyAccount/trac_CloseLoyaltyAccountController.js
@@ -0,0 +1,34 @@
+({
+
+    init: function (component, event, helper) {
+        var status = component.get('v.loyalty.status')
+        if(status == 'paused')
+            component.set('v.isClose', true);
+    },
+
+   openModel: function(component, event, helper) {
+      // Set isModalOpen attribute to true
+      component.set("v.isModalOpen", true);
+   },
+  
+   closeModel: function(component, event, helper) {
+      // Set isModalOpen attribute to false  
+      component.set("v.isModalOpen", false);
+   },
+  
+   closeAccount: function(component, event, helper) {
+
+      if(component.get('v.isMerkleError') == true) {
+          component.set("v.isMerkleError", false);
+          component.set("v.isModalOpen", true);
+      }
+
+      var loyalty = component.get('v.loyalty')
+      var points = loyalty.balance <= 0 ? 0 : -Math.abs(loyalty.balance)
+
+      helper.updateLoyaltyPoints(component, event, helper, loyalty, points, 'close_member')
+      .then(() => helper.pauseAccount(component, event, helper, loyalty))
+      .catch(error => helper.handleError(component, error))
+
+   }
+})
