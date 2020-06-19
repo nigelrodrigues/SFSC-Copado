bb8ea55f954355f6756297b02ef5398afbcb6fa7
Nigel Rodrigues <nrodrigues@tractionondemand.com>
2020-06-19 18:24:49 +0000

    US-0000093: Open a Loyalty Servicing Case

---
+++ aura/trac_EscalateToLoyaltyTeam/trac_EscalateToLoyaltyTeamController.js
@@ -0,0 +1,109 @@
+/**
+ * Created by nrodrigues on 6/10/2020.
+ */
+
+({
+    openForm : function(component, event, helper)
+    {
+        component.set("v.openLightningForm", true);
+    },
+
+    closeForm : function(component, event, helper)
+    {
+        component.set("v.openLightningForm", false);
+    },
+
+    openLightningForm : function (component, event, helper)
+    {
+        component.set("v.isSpinner", false);
+        component.set("v.isLoading", false);
+        component.set("v.openLightningForm", false);
+    },
+
+    handleLoad: function (component, event, helper)
+    {
+        component.find("caseStatus").set("v.value", "Open");
+        var selectedRadioValue = component.get("v.selectedRadioVal");
+        if( selectedRadioValue === 'operations')
+        {
+            component.find("caseType").set("v.value", "Rewards Escalation");
+            component.find("caseCategory").set("v.value", null);
+        }
+        else
+        {
+            component.find("caseType").set("v.value", "Rewards");
+            component.find("caseCategory").set("v.value", "Goodwill Points");
+            component.set("v.rewardsSelected", true);
+            component.set("v.disableCategory", true);
+        }
+
+    },
+    handleError: function (component, event, helper) {
+        component.set("v.isSpinner", false);
+        component.set("v.isLoading", false);
+        console.error('ERROR:' + event.getParam("output").fieldErrors);
+    },
+
+    handleSubmit: function (component, event, helper)
+    {
+
+        var fileUploaded = component.find("fileUpload");
+        if ( fileUploaded && fileUploaded.get("v.files") && fileUploaded.get("v.files").length > 0) {
+            helper.attachFile(component, event);
+        }
+
+        component.set("v.isSpinner", true);
+        component.set("v.isLoading", true);
+    },
+
+    handleSuccess: function (component, event, helper)
+    {
+        component.set("v.isSpinner", false);
+        component.set("v.isLoading", false);
+        component.set("v.openLightningForm", false);
+        var errorEncountered = component.get("v.isError");
+
+        if( !errorEncountered )
+        {
+            helper.showToast('Success', 'The Case will be transferred to the Loyalty Team', 'success');
+
+            const workspaceAPI = component.find("workspace");
+            workspaceAPI.getEnclosingTabId().then(function (tabId) {
+                workspaceAPI.closeTab({tabId});
+            })
+                .catch(function (error) {
+                    console.error('Error in closing tab:' + error);
+                });
+        }
+    },
+
+    handleFilesChange: function (component, event, helper)
+    {
+        var fileName = 'No File Selected..';
+        if (event.getSource().get("v.files").length > 0)
+        {
+            fileName = event.getSource().get("v.files")[0]['name'];
+        }
+        component.set("v.fileName", fileName);
+    },
+
+    handleRadioChange: function (component, event, helper)
+    {
+        const step = event.getSource().get("v.value");
+        console.log('step: ' + step);
+
+        if (step === "operations")
+        {
+            component.find("caseType").set("v.value", "Rewards Escalation");
+            component.set("v.rewardsSelected", false);
+            component.set("v.disableCategory", false);
+        }
+        if (step === "escalation")
+        {
+            component.find("caseType").set("v.value", "Rewards");
+            component.find("caseCategory").set("v.value", "Goodwill Points");
+            component.set("v.disableCategory", true);
+            component.set("v.rewardsSelected", true);
+        }
+    }
+});
---
+++ aura/trac_LoyaltySearch/trac_LoyaltySearchController.js
@@ -0,0 +1,46 @@
+({
+    init: function (component, event, helper) {
+        component.set('v.options', [
+            { id: 1, label: 'Email', selected: true },
+            { id: 2, label: 'Loyalty Account Number' }
+            // { id: 3, label: 'Phone Number' }
+        ]);
+        if (component.get('v.refreshSearch')) {
+            component.set('v.refreshSearch', false);
+            component.set('v.customerLoyaltyId', component.get('v.loyalty.external_customer_id'));
+        }
+        let customerLoyaltyId = component.get('v.customerLoyaltyId');
+        console.log('customerLoyaltyId: ' + customerLoyaltyId);
+        if( customerLoyaltyId )
+        {
+            component.set("v.selectedValue", "2");
+            component.find("loyaltyNumberInput").set("v.value", customerLoyaltyId);
+            helper.getLoyalty(component, helper, null, customerLoyaltyId, null);
+        }
+        else
+        {
+            component.set('v.selectedValue', '1');
+        }
+    },
+
+    loyaltySearch: function (component, event, helper) {
+
+        var email = null;
+        var loyaltyId = null;
+        var phoneNum = null;
+
+
+        if(component.get('v.selectedValue') == '1' && helper.isNotBlank(component, 'emailInput')) {
+            email = component.find('emailInput').get("v.value");
+        } else if (component.get('v.selectedValue') == '2' && helper.isNotBlank(component, 'loyaltyNumberInput')) {
+            loyaltyId = component.find('loyaltyNumberInput').get("v.value");
+        } else if (component.get('v.selectedValue') == '3' && helper.isNotBlank(component, 'phoneNumberInput')) {
+            phoneNum = component.find('phoneNumberInput').get("v.value");
+        }
+        console.log('loyaltyInput: ' + loyaltyId);
+        helper.getLoyaltyUAD(component, helper, email, loyaltyId, phoneNum)
+        .then(() => helper.getLoyalty(component, helper, email, loyaltyId, phoneNum))
+        .catch(error => helper.handleError(component, helper, error))
+
+    },
+});
