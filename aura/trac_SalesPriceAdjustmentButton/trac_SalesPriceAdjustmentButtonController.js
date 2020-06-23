9fd5571b21ad90b67d83b194228eb729a677ea37
Naveen Korandla II <naveen.korandla@hbc.com>
2020-06-23 06:15:36 +0000

    Copado Backup

--- aura/trac_SalesPriceAdjustmentButton/trac_SalesPriceAdjustmentButtonController.js
+++ aura/trac_SalesPriceAdjustmentButton/trac_SalesPriceAdjustmentButtonController.js
@@ -8,5 +8,8 @@
 
     handleCloseModal: function(component, event, helper) {
         component.set("v.isModalOpen", false);
+    },
+    openOldSpa: function(component, event, helper) {
+        window.open('http://jxn-ms-ccwebx1/SPA/Input.aspx', '_blank');
     }
 })
