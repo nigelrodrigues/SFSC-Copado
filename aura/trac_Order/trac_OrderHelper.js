ac2f5a7b8602cef0b02f2310c890df56fbda5ce9
Bryan Cantos <bryan.cantos@hbc.com>
2020-08-03 16:05:00 +0000

    US-0000818: Dev6 to qa1 changes

--- aura/trac_Order/trac_OrderHelper.js
+++ aura/trac_Order/trac_OrderHelper.js
@@ -43,5 +43,54 @@
         }
 
         component.set("v.validSpaBusinessUnit", validSpaBusinessUnit);
+    },
+
+    setBody : function (component, event, helper, newCmp, status, errorMessage)
+    {
+        if (status === "SUCCESS")
+        {
+            let body = component.get("v.body");
+            body.push(newCmp);
+            component.set("v.body", body);
+        }
+        else if (status === "INCOMPLETE")
+        {
+            console.log("No response from server or client is offline.")
+        }
+        else if (status === "ERROR")
+        {
+            console.log("Error: " + errorMessage);
+        }
+    },
+
+    setChannel : function(component, event, helper, order) {
+        var action = component.get("c.getOrderNumberRange");
+
+        component.set("v.isLoading", true);
+
+        action.setParams({
+            "label": 'Digital Blue Martini'
+        });
+
+        action.setCallback(this, function(response) {
+            if (response.getState() == "SUCCESS") {
+                var rangeStr = response.getReturnValue();
+                var ranges = rangeStr.split("-");
+
+                if( helper.isBetweenRange(rangeStr, order.OrderNo) ) {
+                    component.set("v.channel", "Digital Blue Martini");
+                } else {
+                    component.set("v.channel", "Digital");
+                }
+
+            }
+            component.set("v.isLoading", false);
+        });
+        $A.enqueueAction(action);
+    },
+
+    isBetweenRange : function (rangeStr, number) {
+        var ranges = rangeStr.split("-");
+        return (ranges[0] <= number && ranges[1] >= number ) ? true : false
     }
 })
