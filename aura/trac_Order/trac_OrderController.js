5364309966efd05289409c94cfad75fc59d20334
Nigel Rodrigues <nrodrigues@tractionondemand.com>
2020-08-07 18:38:24 +0000

    US-0000826: missing ACTIVE HOLD badge next to line item

--- aura/trac_Order/trac_OrderController.js
+++ aura/trac_Order/trac_OrderController.js
@@ -5,8 +5,36 @@
     doInit : function (component, event, helper) {
         helper.setUnresolvedHolds(component, event, helper);
         helper.spaBusinessUnit(component, event, helper);
-    },
+        helper.setCancelabilityMap(component, event, helper);
+        helper.setActiveHold(component, event, helper);
 
+        // For Cancel button in Order Actions
+        let order = component.get("v.order");
+        let businessUnit = component.get("v.businessUnit");
+        //let orderlineItems = order.OrderLines.OrderLine;
+        if( order.Status === 'Released' ||
+            order.Status === 'Backordered' ||
+            order.Status === 'Hold Credit' ||
+            order.Status === 'Hold Risk' ||
+            order.Status === 'General Error' ||
+            order.Status === 'Created' ||
+            order.Status === 'Open' ||
+            order.Status === 'Scheduled')
+        {
+            component.set("v.disableCancelBtn", false);
+        }
+        if(businessUnit && (businessUnit == 'Off 5th' || businessUnit == 'Saks'))
+        {
+            if (order.Status === 'Released') {
+                component.set("v.disableCancelBtn", true);
+            }
+        }
+        if(order.EntryType === 'POS') {
+            component.set("v.channel", 'Saks CNCT');
+        } else {
+            helper.setChannel(component, event, helper, order)
+        }
+    },
     showOrderLineItems : function (component, event, helper) {
         if(component.get("v.showLineItem")){
             component.set("v.showLineItem", false);
@@ -15,21 +43,17 @@
             component.set("v.showLineItem", true);
         }
     },
-
     handleImport : function (component, event, helper) {
         var action = component.get("c.updateCase");
         var caseRecord = component.get("v.caseRecord");
         var order = component.get("v.order");
-
         action.setParams({
             "caseId": caseRecord.Id,
             "orderNo": order.OrderNo,
             "orderZipCode": (order.PersonInfoBillTo) ? order.PersonInfoBillTo.ZipCode : null
         });
-
         action.setCallback(this, function (response) {
             var state = response.getState();
-
             if (component.isValid() && state === "SUCCESS") {
                 $A.get('e.force:refreshView').fire();
             }
@@ -37,10 +61,8 @@
                 console.log("failed with state: " + state);
             }
         });
-
         $A.enqueueAction(action);
     },
-
     handleAdditionalInfo: function (component, event, helper) {
         if (component.get("v.showAdditionalInfo")) {
             component.set("v.showAdditionalInfo", false);
@@ -48,6 +70,104 @@
         else {
             component.set("v.showAdditionalInfo", true);
         }
+    },
+    handleActions : function (component, event, helper) {
+        let selectedAction = event.getParam("value");
+        let order = component.get("v.order");
+        let caseRecord = component.get("v.caseRecord");
+        let businessUnit = component.get("v.businessUnit");
+        let recordId = component.get("v.caseRecord.Id");
+        switch(selectedAction)
+        {
+            case "cancelOrder":
+                $A.createComponent(
+                    "c:trac_CancelOrder",
+                    {
+                        "isModalOpen": true,
+                        "order": order,
+                        "caseRecord": caseRecord,
+                        "businessUnit": businessUnit,
+                        "showButton": false
+                    },
+                    function(newCmp, status, errorMessage)
+                    {
+                        helper.setBody(component, event, helper, newCmp, status, errorMessage);
+                    }
+                );
+                break;
+            case "addNote":
+                $A.createComponent(
+                    "c:trac_AddNote",
+                    {
+                        "isModalOpen": true,
+                        "order": order,
+                        "caseRecord": caseRecord,
+                        "businessUnit": businessUnit,
+                        "showButton": false
+                    },
+                    function(newCmp, status, errorMessage)
+                    {
+                        helper.setBody(component, event, helper, newCmp, status, errorMessage);
+                    }
+                );
+                break;
+            case "salesPriceAdjustment":
+                $A.createComponent(
+                    "c:trac_SalesPriceAdjustmentButton",
+                    {
+                        "isModalOpen": true,
+                        "order": order,
+                        "caseRecord": caseRecord,
+                        "recordId": recordId,
+                        "showButton": false
+                    },
+                    function(newCmp, status, errorMessage)
+                    {
+                        helper.setBody(component, event, helper, newCmp, status, errorMessage);
+                    }
+                );
+                break;
+            case "orderRefundCredit":
+                let action = component.find("componentORC");
+                component.find('componentORC').set('v.caseRecord', caseRecord);
+                component.find('componentORC').set('v.order', order);
+                component.find('componentORC'). set('v.showButton', false);
+                action.createORC();
+                break;
+            case "paymentCapture":
+                $A.createComponent(
+                    "c:trac_PaymentCapture",
+                    {
+                        "isModalOpen": true,
+                        "order": order,
+                        "caseRecord": caseRecord,
+                        "recordId": recordId,
+                        "showButton": false
+                    },
+                    function(newCmp, status, errorMessage)
+                    {
+                        helper.setBody(component, event, helper, newCmp, status, errorMessage);
+                    }
+                );
+                break;
+            case "orderReturnFee":
+                $A.createComponent(
+                    "c:trac_OrderReturnFee",
+                    {
+                        "isModalOpen": true,
+                        "order": order,
+                        "caseRecord": caseRecord,
+                        "showButton": false
+                    },
+                    function(newCmp, status, errorMessage)
+                    {
+                        helper.setBody(component, event, helper, newCmp, status, errorMessage);
+                    }
+                );
+                break;
+            case "linkToCase":
+                this.handleImport(component, event, helper);
+                break;
+        }
     }
-
 })
