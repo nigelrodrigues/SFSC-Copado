93f2344bdacc06fc5280083ef389d89b03f2c2c0
Bryan Cantos <bryan.cantos@hbc.com>
2020-08-12 15:08:01 +0000

    US-0000848: [UAD] Ability to service international orders

--- aura/trac_Order/trac_OrderHelper.js
+++ aura/trac_Order/trac_OrderHelper.js
@@ -18,7 +18,6 @@
         });
         resultsToast.fire();
     },
-
     setUnresolvedHolds: function (component, event, helper) {
         var order = component.get("v.order");
         var hasUnresolvedHolds = false;
@@ -33,15 +32,86 @@
         }
         component.set("v.hasUnresolvedHolds", hasUnresolvedHolds);
     },
-
     spaBusinessUnit: function (component, event, helper) {
         var order = component.get("v.order");
         let validSpaBusinessUnit = false;
-
         if(order && order.EnterpriseCode && (order.EnterpriseCode === ('SAKS') || order.EnterpriseCode === ('OFF5'))) {
             validSpaBusinessUnit = true;
         }
-
         component.set("v.validSpaBusinessUnit", validSpaBusinessUnit);
-    }
+    },
+
+
+    setBody : function (component, event, helper, newCmp, status, errorMessage)
+    {
+        if (status === "SUCCESS")
+        {
+            let body = component.get("v.body");
+            body.push(newCmp);
+            component.set("v.body", body);
+        }
+        else if (status === "INCOMPLETE")
+        {
+            console.log("No response from server or client is offline.")
+        }
+        else if (status === "ERROR")
+        {
+            console.log("Error: " + errorMessage);
+        }
+    },
+    setChannel : function(component, event, helper, order) {
+        var action = component.get("c.getOrderNumberRange");
+        component.set("v.isLoading", true);
+        action.setParams({
+            "label": 'Digital Blue Martini'
+        });
+        action.setCallback(this, function(response) {
+            if (response.getState() == "SUCCESS") {
+                var rangeStr = response.getReturnValue();
+                var ranges = rangeStr.split("-");
+                if( helper.isBetweenRange(rangeStr, order.OrderNo) ) {
+                    component.set("v.channel", "Digital Blue Martini");
+                } else {
+                    component.set("v.channel", "Digital");
+                }
+
+            }
+            component.set("v.isLoading", false);
+        });
+        $A.enqueueAction(action);
+    },
+    setCancelabilityMap: function(component, event, helper) {
+        console.log('Inside setCancelabilityMap');
+        var businessUnit = component.get('v.businessUnit');
+        var action = component.get("c.getOrderLineCancelabilityByStatus");
+        action.setParams({
+            "businessUnit": businessUnit
+        });
+        action.setCallback(this, function(response) {
+            if (response.getState() == "SUCCESS") {
+                console.log('response.getReturnValue(): ', response.getReturnValue());
+                component.set('v.cancelabilityMap', response.getReturnValue());
+            }
+
+        });
+        $A.enqueueAction(action);
+    },
+    isBetweenRange : function (rangeStr, number) {
+        var ranges = rangeStr.split("-");
+        return (ranges[0] <= number && ranges[1] >= number ) ? true : false
+    },
+    setActiveHold: function(component)
+    {
+        let order = component.get("v.order");
+        if(order.OrderHoldTypes.OrderHoldType)
+        {
+            for (let orderHoldType of order.OrderHoldTypes.OrderHoldType)
+            {
+                if(orderHoldType.StatusDescription === 'Created')
+                {
+                    component.set("v.isActiveHold", true);
+                }
+            }
+        }
+    },
 })
