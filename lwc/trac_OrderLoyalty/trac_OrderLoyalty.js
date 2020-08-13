76301161b1fc94d4a7fa40882e4776ad81fa0990
Admin Traction <admin@tractionondemand.com>
2020-08-13 12:51:25 +0000

    US-0000849: Prod deployment test class fix

---
+++ lwc/trac_OrderLoyalty/trac_OrderLoyalty.js
@@ -0,0 +1,11 @@
+/**
+ * Created by akong on 6/17/2020.
+ */
+
+import {LightningElement, api} from 'lwc';
+
+export default class TracOrderLoyalty extends LightningElement {
+
+    @api order;
+
+}
