e85be0ce88217fe2fc891a227c39d6f5739a2e38
Admin Traction <admin@tractionondemand.com>
2020-08-14 19:17:46 +0000

    US-0000856: Saks Stage UAD: Prices for Domestic Orders are Displayed in CA$ in O

--- aura/trac_Loyalty/trac_LoyaltyController.js
+++ aura/trac_Loyalty/trac_LoyaltyController.js
@@ -1,15 +1,17 @@
+/**
+ * Created by akong on 6/1/2020.
+ */
+
 ({
     handleShowAppeasementModal: function(cmp, event, helper) {
         cmp.set('v.showAppeasementModal', true);
     },
-
     handleCloseModalApplicationEvent: function(cmp) {
         cmp.set('v.showAppeasementModal', false);
     },
     handleShowRecordTransaction: function(cmp,event) {
         let openButton = event.getSource();
         let container = cmp.find("divBody");
-
         $A.createComponent(
             "c:trac_RecordTransaction",
             {
@@ -38,34 +40,24 @@
         })
         .catch(error => helper.handleError(component, helper, error))
     },
-
-
     handleChange: function(component, event, helper) {
         var firstNameValid = component.find('firstNameInput').get("v.validity")
         var lastNameValid = component.find('lastNameInput').get("v.validity")
         var emailValid = component.find('emailInput').get("v.validity")
-
-
         if(firstNameValid.valid && lastNameValid.valid && emailValid.valid) {
             component.set('v.isDisabled', false)
         } else {
             component.set('v.isDisabled', true)
         }
     },
-
-
     handleEditLoyaltyApplicationEvent: function (component, event, helper) {
         var firstName = event.getParam("firstName");
         var lastName = event.getParam("lastName");
         var email = event.getParam("email");
-
-
         component.set('v.firstName', firstName)
         component.set('v.lastName', lastName)
         component.set('v.email', email)
     },
-
-
     handleMerkleErrorEvent : function(cmp, event) {
         var modalId = 'merkleCommonErrorModal';
         var canRetry = event.getParam('canRetry');
@@ -95,6 +87,4 @@
                 }
             });
     },
-
-
 });
